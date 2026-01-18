44c003ebb0575638f80847d468137dfd4fa1613a
mke-l <mikeli2007193@gmail.com>
2026-01-17 17:01:30 -0800

    Addtional Endpoints

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -8,6 +8,7 @@ userRouter.post("/:userId/newcontract", userController.createContract);
 
 // View profile
 userRouter.get("/:userId", userController.getUserProfile);
+userRouter.get("/by-email", userController.getUserByEmail);
 
 // Edit profile (partial updates)
 userRouter.patch("/:userId", userController.updateUserProfile);
